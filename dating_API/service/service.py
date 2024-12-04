35ee15f91cc397acadfb33a1be515ddbbcbdf570
Ivan <vanuartw@mail.ru>
2024-12-04 09:42:20 +0300

    изменение пути сохранения изображений интересов

--- dating_API/service/service.py
+++ dating_API/service/service.py
@@ -4,11 +4,11 @@ def loading_interests_images(instance, file):
     Загрузка изображений  интересов
     '''
     file_exten = file.split('.')
-    return f'interests/{instance.slug}.{file_exten}'
+    return f'interests/{instance.slug}.{file}'
 
 
 def loading_profile_images(instance, file):
     '''
     Загрузка изображений профиля аккаунта
     '''
-    return f'profiles/{instance.profile}/{file}'
+    return f'profiles/{instance.profile}.{file}'
