06a223de2b1c033012773717d4fdbf4acb4d8de8
Ivan <vanuartw@mail.ru>
2024-12-04 09:59:21 +0300

    обновление БД

--- dating_API/service/service.py
+++ dating_API/service/service.py
@@ -4,11 +4,11 @@ def loading_interests_images(instance, file):
     Загрузка изображений  интересов
     '''
     file_exten = file.split('.')
-    return f'interests/{instance.slug}.{file}'
+    return f'interests/{instance.slug}_{file}'
 
 
 def loading_profile_images(instance, file):
     '''
     Загрузка изображений профиля аккаунта
     '''
-    return f'profiles/{instance.profile}.{file}'
+    return f'profiles/{instance.profile}_{file}'
